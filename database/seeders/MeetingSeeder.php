0c3c9ae0d806409016d3056dd0eb2ec11c957705
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-02-04 14:59:52 -0300

    [Seeder] Create Seeders - categorySeeder - questionSeeder - meetingSeeder - userSeeder

---
+++ database/seeders/MeetingSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Meeting;
+use Illuminate\Database\Seeder;
+
+class MeetingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Meeting::factory(4)->create();
+    }
+}
