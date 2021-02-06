e6fff53aedb82b5380a47566392aa390389cc7d1
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-02-06 00:58:40 -0300

    [User migration] fixed charset database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('phone')->nullable();
             $table->integer('roles')->default(0);
-            $table->text('bio')->default('Conte um pouco sobre voce');
+            $table->text('bio')->default('bio');
             $table->string('avatar')->default('default-avatar.png');
             $table->rememberToken();
             $table->timestamps();
